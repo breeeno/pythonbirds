2301ff32cb35660d9231aa3e2e47af6ab4697774
Estudo <breno.eustaq@gmail.com>
2021-11-16 22:06:19 -0300

    - criados atributos de instancia nome e idade - criado objeto filhos - criado e removido atributo dinamico de objeto do tipo Pessoa - criado atributo de classe olhos

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos = 2
     def __init__(self, *filhos, nome = None, idade = 19):
         self.idade = idade
         self.nome = nome
@@ -17,5 +18,13 @@ if __name__ == '__main__':
         print(filho.nome)
     walter.sobrenome = 'Eustáquio'
     del walter.filhos
+    breno.olhos = 1
+    del breno.olhos
     print(breno.__dict__)
     print(walter.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(walter.olhos)
+    print(breno.olhos)
+    print(id(Pessoa.olhos), id(walter.olhos), id(breno.olhos))
+
