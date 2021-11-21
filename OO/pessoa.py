54ca09eae5971511a7f323c95020689fb915932c
Estudo <breno.eustaq@gmail.com>
2021-11-21 13:02:05 -0300

    - inserido classe caracter_ativo e caracter_destruido

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -5,7 +5,7 @@ class Pessoa:
         self.nome = nome
         self.filhos = list(filhos)
     def cumprimentar (self):
-        return f'Olá! {id(self)}'
+        return f'Olá! meu nome é {self.nome}'
     @staticmethod
     def metodo_estatico():
         return 42
@@ -14,9 +14,19 @@ class Pessoa:
     def nome_e_atribututos_de_classe(cls):
         return(f'{cls} - olhos {cls.olhos} ')
 
+class Homem(Pessoa):
+    def cumprimentar(self):
+        cumprimentar_da_classe = super().cumprimentar()
+        return f'{cumprimentar_da_classe}, aperto de mão.'
+
+
+class Mutante(Pessoa):
+    olhos = 3
+
 if __name__ == '__main__':
-    breno = Pessoa(nome = 'Breno')
-    walter = Pessoa(breno, nome = 'Walter' )
+    pessoa = Pessoa
+    breno = Homem(nome = 'Breno')
+    walter = Homem(breno, nome = 'Walter' )
     print(Pessoa.cumprimentar(breno))
     print(id(breno))
     print(breno.cumprimentar())
@@ -29,13 +39,19 @@ if __name__ == '__main__':
     del breno.olhos
     print(breno.__dict__)
     print(walter.__dict__)
-    Pessoa.olhos = 3
     print(Pessoa.olhos)
     print(walter.olhos)
     print(breno.olhos)
     print(id(Pessoa.olhos), id(walter.olhos), id(breno.olhos))
     print(Pessoa.metodo_estatico(), breno.metodo_estatico())
     print(Pessoa.nome_e_atribututos_de_classe(), breno.nome_e_atribututos_de_classe())
+    print(isinstance(pessoa, Pessoa))
+    print(isinstance(pessoa, Homem))
+    print(isinstance(breno, pessoa))
+    print(isinstance(breno, Homem))
+    print(breno.cumprimentar())
+    print(walter.cumprimentar())
+
 
 
 
